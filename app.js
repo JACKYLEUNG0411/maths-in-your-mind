da0a57269772eb8b1b7372540a5efd1c0e778d47
JACKYLEUNG0411 <ariesjack411@gmail.com>
2026-09-16 13:53:59 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 
-const SUPABASE_URL = "https://zlafcarhdhtugcmeaqzv.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://zlafcarhdhtugcmeaqzv.supabase.co";
 const SUPABASE_KEY = "sb_publishable_msVVLrRWTYHKAnia15J-Pw_nDdcK3Mb";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
+
 
 
 
