4ad16aa73cd86e0567bf8c6c7a2243634fc28ef9
JACKYLEUNG0411 <ariesjack411@gmail.com>
2026-09-16 17:18:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
+const SUPABASE_URL =
+  "https://zlafcarhdhtugcmeaqzv.supabase.co";
+
+/*
+  只可以使用 Publishable key 或 anon public key。
+  絕對不要把 service_role key 放在這裡。
+*/
+const SUPABASE_KEY =
+  "sb_publishable_msVVLrRWTYHKAnia15J-Pw_nDdcK3Mb";
+
+window.db = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
 
